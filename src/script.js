012e4e21f662ad2b2dbdbae76293ee82a8a2c3b7
Mark Mitri <mame23@lehigh.edu>
2023-05-11 11:55:34 -0400

    endGame only fires if there is a win. Disabling buttons still does not work.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-let board = [[]]; // game board representation
 const row = 9; const col = 9;
 let currentPlayer = "X";
 let arr = initializeBoard(row,col);
@@ -25,10 +24,6 @@ for(let i = 0; i < box.length; i++){
         checkLargeWin(winArr, XWin, OWin, largeTie);
         switchPlayers();
         makeClickable(buttonId);
-        if(XWin || OWin || largeTie){
-            console.log("Ending game...");
-            endGame(XWin, OWin, largeWin);
-        }
     }
 }
 
@@ -59,7 +54,8 @@ function checkSmallWin(arr, buttonId, winArr, smallTie){
         const bigBox = document.getElementById("bigBox"+first);
         var text = "X";
         bigBox.innerHTML = text;
-    }else if(arr[first][0]=="O" && arr[first][1]=="O" && arr[first][2]=="O" || // first small row
+    }
+    if(arr[first][0]=="O" && arr[first][1]=="O" && arr[first][2]=="O" || // first small row
        arr[first][3]=="O" && arr[first][4]=="O" && arr[first][5]=="O" || // second smallrow
        arr[first][6]=="O" && arr[first][7]=="O" && arr[first][8]=="O" || // third small row
        arr[first][0]=="O" && arr[first][3]=="O" && arr[first][6]=="O" || // first small column
@@ -71,7 +67,8 @@ function checkSmallWin(arr, buttonId, winArr, smallTie){
         const bigBox = document.getElementById("bigBox"+first);
         var text = "O";
         bigBox.innerHTML = text;
-    }else{
+    }
+    if(/* condition for tie*/){
         smallTie = true;
     }
 }
@@ -85,9 +82,12 @@ function checkLargeWin(winArr, XWin, OWin, largeTie){
        winArr[2]=="X" && winArr[5]=="X" && winArr[8]=="X" || // third large column
        winArr[0]=="X" && winArr[4]=="X" && winArr[8]=="X" || // large diagonal 1
        winArr[2]=="X" && winArr[4]=="X" && winArr[8]=="X" ){ // large diagonal 2
+        console.log(winArr);
         XWin = true; OWin = false; largeTie = false;
         console.log("XWin: ", XWin); console.log("OWin: ", OWin); console.log("LargeTie: ", largeTie);
-    }else if(winArr[0]=="O" && winArr[1]=="O" && winArr[2]=="O" || // first large row
+        endGame(XWin, OWin, largeTie);
+    }
+    if(winArr[0]=="O" && winArr[1]=="O" && winArr[2]=="O" || // first large row
        winArr[3]=="O" && winArr[4]=="O" && winArr[5]=="O" || // second large row
        winArr[6]=="O" && winArr[7]=="O" && winArr[8]=="O" || // third large row
        winArr[0]=="O" && winArr[3]=="O" && winArr[6]=="O" || // first large column
@@ -95,11 +95,19 @@ function checkLargeWin(winArr, XWin, OWin, largeTie){
        winArr[2]=="O" && winArr[5]=="O" && winArr[8]=="O" || // third large column
        winArr[0]=="O" && winArr[4]=="O" && winArr[8]=="O" || // large diagonal 1
        winArr[2]=="O" && winArr[4]=="O" && winArr[8]=="O" ){ // large diagonal 2
+        console.log(winArr);
         XWin = false; OWin = true; largeTie = false;
         console.log("XWin: ", XWin); console.log("OWin: ", OWin); console.log("LargeTie: ", largeTie);
-    }else{
-        XWin = false; OWin = false; largeTie = true;
-        console.log("XWin: ", XWin); console.log("OWin: ", OWin); console.log("LargeTie: ", largeTie);
+        endGame(XWin, OWin, largeTie);
+    }
+    for (var i = 0; i < winArr.length; i++) {
+        if (winArr[i] === '') {
+            console.log(winArr);
+            console.log("winArr length: ", winArr.length);
+            XWin = false; OWin = false; largeTie = true;
+            console.log("XWin: ", XWin); console.log("OWin: ", OWin); console.log("LargeTie: ", largeTie);
+            endGame(XWin, OWin, largeTie);
+        }
     }
 }
 
